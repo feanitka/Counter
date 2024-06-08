154a190a42b1514aed0d6b20c9ffc95adbb8d05a
anika <Anna.Fedchenko2002@yandex.ru>
2024-06-08 20:46:53 +0300

    проект

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -6,14 +6,25 @@
 //
 
 import UIKit
-
+var counter: Int = 0
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    var count = 0
+    @IBOutlet weak var clickButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        clickButton.tintColor = .systemPink
     }
-
-
+    @IBAction func buttonDidTap(_ sender: Any) {
+        count +=  1
+        
+        counterLabel.text = "\(count)"
+        print("tab")
+    }
+    
 }
 
